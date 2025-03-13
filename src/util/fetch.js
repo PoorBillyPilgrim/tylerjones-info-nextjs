aab9d314f4c4dbb4c99204a298e867dea6dd2061
Tyler Jones <tyler.jones@lyrasis.org>
2025-03-12 20:18:28 -0700

    update next and installed MUI V6

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -1,15 +1,7 @@
 import fs from "fs";
-import { serialize } from "next-mdx-remote/serialize";
-import { compileMDX } from "next-mdx-remote/rsc";
 
 export const getNote = async slug => {
   // const realSlug = slug.replace(/\)
   const noteFile = fs.readFileSync(`src/data/notes/${slug}.mdx`);
   return noteFile;
-  //   const { frontmatter, content } = await compileMDX({
-  //     source: noteFile,
-  //     options: { parseFrontmatter: true },
-  //   });
-
-  //   return { meta: { ...frontmatter, slug }, content };
 };
