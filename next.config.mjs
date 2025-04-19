16b48926f1f7d562ecc7499fe8c8566a86fa707e
Tyler Jones <tyler.jones@lyrasis.org>
2025-04-19 11:41:34 -0400

    basePath should be the same for dev and prod

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@ const isDev = process.env.NODE_ENV === "development";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: isDev ? "" : "/tylerjones-info-nextjs",
+  // basePath: isDev ? "" : "/tylerjones-info-nextjs"
+  basePath: "",
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
