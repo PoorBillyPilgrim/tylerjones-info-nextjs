60a25ca9aab5ec7b16118b5ea9216c8db2adf427
Tyler Jones <tyler.jones@lyrasis.org>
2025-04-19 10:57:35 -0400

    more config testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 import withMDX from "@next/mdx";
 
+const isDev = process.env.NODE_ENV === "development";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/tylerjones-info-nextjs",
-  images: {
-    unoptimized: true,
-  },
+  basePath: isDev ? "" : "/tylerjones-info-nextjs",
+  assetPrefix: isDev ? "" : "/tylerjones-info-nextjs/",
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         hostname: "raw.githubusercontent.com",
@@ -17,5 +18,10 @@ const nextConfig = {
     ],
   },
 };
+
+if (isDev) {
+  delete nextConfig.output;
+  delete nextConfig.images.unoptimized;
+}
 
 export default withMDX()(nextConfig);
