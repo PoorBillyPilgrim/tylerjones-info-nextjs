240c64b29e98fb2553071313288fb3818bf10afd
Tyler Jones <tyler.jones@lyrasis.org>
2025-11-22 16:12:03 -0800

    add updated resume

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,34 +1,31 @@
 import React from "react";
 import Image from "next/image";
 
-import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
 const NotFound = () => {
   return (
-    <Grid
-      container
-      direction="column"
+    <Stack
+      spacing={3}
+      useFlexGap
       sx={{ justifyContent: "center", alignItems: "center", height: "100vh" }}
     >
-      <Grid>
-        <Image
-          alt="old picture of Tyler Jones"
-          src="/images/sixth-grade.jpeg"
-          height={500}
-          width={500}
-        />
-      </Grid>
-      <Grid>
-        <Typography variant="h1">404</Typography>
-      </Grid>
-      <Grid>
-        <Typography>
+      <Image
+        alt="old picture of Tyler Jones"
+        src="/images/sixth-grade.jpeg"
+        height={400}
+        width={400}
+      />
+
+      <Stack sx={{ justifyContent: "center", alignItems: "center" }}>
+        <Typography sx={{ fontSize: "4rem" }}>404</Typography>
+        <Typography sx={{ fontSize: "1.5rem" }}>
           Sorry, don't have this page. Here's my first day of 6th grade for your
           troubles.
         </Typography>
-      </Grid>
-    </Grid>
+      </Stack>
+    </Stack>
   );
 };
 
